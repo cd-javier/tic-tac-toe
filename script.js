63947cdb780211d3a56e627d8b6ba33deefc9426
Javier Quiroga <contact@javierquiroga.com>
2024-12-13 20:52:03 +0000

    Add hover effect on cells

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function GameFlow() {
     announcePlayer();
   })();
 
-  return { playRound, getBoard, getDisplayMessage };
+  return { playRound, getBoard, getDisplayMessage, players };
 }
 
 (function UI() {
@@ -227,16 +227,55 @@ function GameFlow() {
   // IIFE that contains all event listeners
   (function EventListeners() {
     Queries.board.addEventListener("click", markSquare);
+    Queries.board.addEventListener("mouseover", startHover);
+    Queries.board.addEventListener("mouseout", endHover);
     Queries.resetBtn.addEventListener("click", resetGame);
   })();
 
   // When the board is clicked, if the click is done on a cell,
   // the playRound function is called with the values X and Y of that cell
   function markSquare(e) {
-    targetSquare = e.target;
+    const targetSquare = e.target;
     if (targetSquare.classList.contains("cell")) {
       game.playRound(targetSquare.dataset.x, targetSquare.dataset.y);
       render();
+    }
+  }
+
+  // When the mouse enters a square that hasn't been played,
+  // the symbol of that player is showed with less opacity
+  function startHover(e) {
+    const targetSquare = e.target;
+    if (
+      targetSquare.classList.contains("cell") &&
+      !targetSquare.classList.contains("p1") &&
+      !targetSquare.classList.contains("p2")
+    ) {
+      const activePlayer = game.players.getActivePlayer().symbol;
+      switch (activePlayer) {
+        case 1:
+          targetSquare.classList.add("p1-hover");
+          break;
+        case 2:
+          targetSquare.classList.add("p2-hover");
+          break;
+      }
+    }
+  }
+
+  // When the mouse leaves the square, the symbol disappears
+  function endHover(e) {
+    const targetSquare = e.target;
+    if (targetSquare.classList.contains("cell")) {
+      const activePlayer = game.players.getActivePlayer().symbol;
+      switch (activePlayer) {
+        case 1:
+          targetSquare.classList.remove("p1-hover");
+          break;
+        case 2:
+          targetSquare.classList.remove("p2-hover");
+          break;
+      }
     }
   }
 
